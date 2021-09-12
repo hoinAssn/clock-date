032fce11e84eda8267a8ede80124c94b85c815f7
hoinAssn <hrold060524@gmail.com>
2021-09-12 12:57:38 +0900

    tt

--- clock, date/소스.c
+++ clock, date/소스.c
@@ -7,6 +7,7 @@
 
 void hour(void);
 void date(void);
+int leapyear(int);
 void printscreen(void);
 
 int main() {
@@ -29,11 +30,20 @@ void hour(void) {
 }
 
 void date(void) {
-    int year, month, chk = 0;
+    int year, month;
+    int chk = 0, sum = 0, k = 0;
     int basicyear[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 
 	printf("\n\n몇년 몇월을 볼지 입력해주세요\n\nex)2018 03 >> ");
 	scanf_s("%d %d", &year, &month, sizeof(year), sizeof(month));
+
+    if (month > 12 || month < 1 || year < 0) {
+        printf("\n입력하신 날짜가 형식에 맞지 않습니다\n다시 입력해주세요");
+        Sleep(3000);
+        system("cls");
+        date();
+        return 0;
+    }
 
     if (leapyear(year)) {
         chk = 1;
@@ -42,13 +52,19 @@ void date(void) {
     else
         chk = 0;
 
-	if (month > 12 || month < 1) {
-		printf("\n입력하신 날짜가 형식에 맞지 않습니다\n다시 입력해주세요");
-		Sleep(3000);
-		system("cls");
-		date();
-		return 0;
-	}
+    sum = 365;
+    for (int i = 1; i < year; i++) {
+        if (leapyear(i))
+            sum += 366;
+        else
+            sum += 365;
+    }
+
+    for (int i = 0; i < month - 1; i++) {
+        sum += basicyear[i];
+    }
+
+    k = sum % 7;
 
 	Sleep(500);
 	system("cls");
@@ -58,12 +74,22 @@ void date(void) {
 	printscreen();
 }
 
+int leapyear(int a) {
+    if ((a % 4 == 0) && !(a % 100 == 0) || (a % 400 == 0)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 void printscreen(void) {
 	printf("     ---------------------------------------------------------------------------------------------------     ");
 	printf("     |     SUN     |     MON     |     TUE     |     WED     |     THU     |     FRI     |     SAT     |     ");
 	printf("     ---------------------------------------------------------------------------------------------------     ");
 
 }
+
 /*#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
